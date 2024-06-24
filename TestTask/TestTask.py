70be71a1b251ce80f1148a13e1dd298c0b72c60a
Ivana Piačková <yxwik13@gmail.com>
2024-06-24 20:33:41 +0200

    sync - updated files and changes inside subdirectories

--- TestTask/TestTask.py
+++ TestTask/TestTask.py
@@ -1,4 +1,3 @@
-from genericpath import isdir
 import os
 import sys
 import time
@@ -14,25 +13,39 @@ def sync_fun(source, replica):
         file_path = os.path.join(source, file)
         target_path = os.path.join(replica, file)
         
-        if os.path.isdir(file_path):             #subdirectory
+        if os.path.isdir(file_path):                #subdirectory
             shutil.copytree(file_path, target_path)
             print(f"Copied new subdirectory {file_path} to {target_path}")
         else:
-            shutil.copy2(file_path, target_path)     #file
+            shutil.copy2(file_path, target_path)    #file
             print(f"Copied new file {file_path} to {target_path}")
             
     #removed files or subdirectories
     for file in comp.right_only:
         file_path = os.path.join(replica, file)
         
-        if os.path.isdir(file_path):             #subdirectory
+        if os.path.isdir(file_path):                #subdirectory
             shutil.rmtree(file_path)
             print(f"Removed subdirectory {file_path}")
         else:
-            os.remove(file_path)                #file
+            os.remove(file_path)                    #file
             print(f"Removed file {file_path}")        
     
+    #updated files
+    for file in comp.diff_files:
+        file_path = os.path.join(source, file)
+        target_path = os.path.join(replica, file)
         
+        shutil.copy2(file_path, target_path) 
+        print(f"Updated file {file_path} to {target_path}")
+        
+    #updated subdirectories - include file creation and removal within subdirectory and change of files
+    for subdir in comp.common_dirs:
+        source_subdir_path = os.path.join(source, subdir)
+        target_subdir_path = os.path.join(replica, subdir)
+        sync_fun(source_subdir_path, target_subdir_path)  
+
+#----------------------------------------------------------------------------------------------------------------
 
 #arguments - folder paths, synchronization interval, log file path
 if len(sys.argv) != 5:
@@ -53,10 +66,7 @@ sync_int = int(sys.argv[3])
 log_file_path = sys.argv[4]
 
 #synchronization process
-max_num = 0
 while(True):
     sync_fun(source_path, replica_path)
-    max_num += 1
-    if max_num == 5:
-        sys.exit()
+
     time.sleep(sync_int)
