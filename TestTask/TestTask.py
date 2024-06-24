408211ccea8d598d34c39ffdb97131b15288dddd
Ivana Piačková <yxwik13@gmail.com>
2024-06-24 10:32:52 +0200

    setting input arguments

--- TestTask/TestTask.py
+++ TestTask/TestTask.py
@@ -1,1 +1,20 @@
+import os
+import sys
 
+#arguments - folder paths, synchronization interval, log file path
+if len(sys.argv) != 5:
+    print("Wrong number of arguments!")
+    sys.exit()
+
+source = sys.argv[1]
+# while not os.path.isdir(source):
+#     print("The source folder does not exist!")
+#     sys.exit()
+
+replica = sys.argv[2]
+# while not os.path.isdir(replica):
+#     print("The replica folder does not exist!")
+#     sys.exit()
+
+sync_int = sys.argv[3]
+log_file_path = sys.argv[4]
