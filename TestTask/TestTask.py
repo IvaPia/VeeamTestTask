0323f91f3823980ebc9e807ec8f955579097cb47
Ivana Piačková <yxwik13@gmail.com>
2024-06-25 12:03:12 +0200

    write changes to log file

--- TestTask/TestTask.py
+++ TestTask/TestTask.py
@@ -3,8 +3,9 @@ import sys
 import time
 import filecmp
 import shutil
+import datetime
 
-def sync_fun(source, replica):
+def sync_fun(source, replica, log_file):
     #compare folders
     comp = filecmp.dircmp(source, replica)
     
@@ -16,9 +17,11 @@ def sync_fun(source, replica):
         if os.path.isdir(file_path):                #subdirectory
             shutil.copytree(file_path, target_path)
             print(f"Copied new subdirectory {file_path} to {target_path}")
+            log_file.write(f"Copied new subdirectory {file_path} to {target_path} at {datetime.datetime.now()}\n")
         else:
             shutil.copy2(file_path, target_path)    #file
             print(f"Copied new file {file_path} to {target_path}")
+            log_file.write(f"Copied new file {file_path} to {target_path} at {datetime.datetime.now()}\n")
             
     #removed files or subdirectories
     for file in comp.right_only:
@@ -27,9 +30,11 @@ def sync_fun(source, replica):
         if os.path.isdir(file_path):                #subdirectory
             shutil.rmtree(file_path)
             print(f"Removed subdirectory {file_path}")
+            log_file.write(f"Removed subdirectory {file_path} at {datetime.datetime.now()}\n")
         else:
             os.remove(file_path)                    #file
-            print(f"Removed file {file_path}")        
+            print(f"Removed file {file_path}")
+            log_file.write(f"Removed file {file_path} at {datetime.datetime.now()}\n")
     
     #updated files
     for file in comp.diff_files:
@@ -37,13 +42,14 @@ def sync_fun(source, replica):
         target_path = os.path.join(replica, file)
         
         shutil.copy2(file_path, target_path) 
-        print(f"Updated file {file_path} to {target_path}")
+        print(f"Updated file {file_path} and copied to {target_path}")
+        log_file.write(f"Updated file {file_path} and copied to {target_path} at {datetime.datetime.now()}\n")
         
     #updated subdirectories - include file creation and removal within subdirectory and change of files
     for subdir in comp.common_dirs:
         source_subdir_path = os.path.join(source, subdir)
         target_subdir_path = os.path.join(replica, subdir)
-        sync_fun(source_subdir_path, target_subdir_path)  
+        sync_fun(source_subdir_path, target_subdir_path, log_file)  
 
 #----------------------------------------------------------------------------------------------------------------
 
@@ -65,8 +71,12 @@ replica_path = sys.argv[2]
 sync_int = int(sys.argv[3])
 log_file_path = sys.argv[4]
 
+#create a log file
+f = open(os.path.join(log_file_path, "sync_log_file.txt"), "w")
+f.write(f"Folder synchronization between source {source_path} and replica {replica_path} started at {datetime.datetime.now()}\n\n")
+
 #synchronization process
 while(True):
-    sync_fun(source_path, replica_path)
+    sync_fun(source_path, replica_path, f)
 
     time.sleep(sync_int)
